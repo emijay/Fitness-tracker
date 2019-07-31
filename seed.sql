c01e5c4df4089a92f234e135df55dc1506150039
emijay <hilmi.johari@yahoo.com.sg>
2019-07-31 15:02:39 +0800

    minor changes to add some more columns to exercise table

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO workoutType (name) VALUES ('Arms');
+INSERT INTO workoutType (name) VALUES ('Back');
+INSERT INTO workoutType (name) VALUES ('Chest');
+INSERT INTO workoutType (name) VALUES ('Legs');
+INSERT INTO workoutType (name) VALUES ('Cardio');
+INSERT INTO workoutType (name) VALUES ('Yoga');
--- tables.sql
+++ tables.sql
@@ -11,18 +11,20 @@ CREATE TABLE IF NOT EXISTS workoutType (
 	name TEXT
 );
 
-CREATE TABLE NOT EXISTS exercises (
+CREATE TABLE IF NOT EXISTS exercises (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
 	weight INTEGER,
 	reps INTEGER,
 	sets INTEGER,
+	distance INTEGER,
+	duration INTEGER,
 	workout_id INTEGER,
 	user_id INTEGER,
 	created_at DATE DEFAULT now()
 );
 
-CREATE TABLE NOT EXISTS macros (
+CREATE TABLE IF NOT EXISTS macros (
 	id SERIAL PRIMARY KEY,
 	carbs INTEGER,
 	protein INTEGER,
@@ -32,7 +34,7 @@ CREATE TABLE NOT EXISTS macros (
 	created_at DATE DEFAULT now()
 );
 
-CREATE TABLE NOT EXISTS bodystats (
+CREATE TABLE IF NOT EXISTS bodystats (
 	id SERIAL PRIMARY KEY,
 	height INTEGER,
 	weight INTEGER,
